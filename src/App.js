663547175c686caf42500a4f9e903565cc2d7e5c
Audrey Li <me@audreyli.me>
2019-08-18 13:45:19 -0700

    S2: cube object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import Worldview, { Text } from "regl-worldview";
+import Worldview, { Axes, Cubes, Grid } from "regl-worldview";
 import "./App.css";
 
 function App() {
+  const OBJECT_COUNT = 100;
+  const cubes = new Array(OBJECT_COUNT).fill().map((_, idx) => ({
+    pose: {
+      position: { x: idx, y: idx, z: idx },
+      orientation: { x: 0, y: 0, z: 0, w: 1 }
+    },
+    scale: { x: 1 * Math.random(), y: 1, z: 1 },
+    color: { r: Math.random(), g: Math.random(), b: Math.random(), a: 1 }
+  }));
+
   return (
     <div className="App" style={{ width: "100vw", height: "100vh" }}>
       <Worldview>
-        <Text>
-          {[
-            {
-              text: "Welcome to Cruise!",
-              pose: {
-                position: { x: 0, y: 0, z: 0 },
-                orientation: { x: 0, y: 0, z: 0, w: 1 }
-              },
-              color: { r: 0.92, g: 0.094, b: 0, a: 1 }
-            }
-          ]}
-        </Text>
+        <Cubes>{cubes}</Cubes>
+        <Grid />
+        <Axes />
       </Worldview>
     </div>
   );
